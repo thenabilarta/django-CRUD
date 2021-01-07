0d4a0a84084fc81c2219c3fb9751a52aa96ecf2c
thenabilarta <thenabilarta@gmail.com>
2021-01-08 00:57:28 +0700

    Show database to the view

--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from .models import *
+
 
 def home(request):
     return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {"products": products})
 
 
 def customer(request):
